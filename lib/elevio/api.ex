62bfb6777cbc50c1e37dc33f016fed2a26dc8731
jmramos02 <ramosjosemari@gmail.com>
2019-07-01 22:04:07 +0800

    Add headers

---
+++ lib/elevio/api.ex
@@ -0,0 +1,15 @@
+defmodule Elevio.Api do
+  use HTTPoison.Base
+
+  def process_request_url(url) do
+    Application.get_env(:elevio, Elevio.API)[:elevio_base_url] <> url
+  end
+
+  def process_request_headers(headers) do
+    token = Application.get_env(:elevio, Elevio.API)[:elevio_api_secret]
+    key = Application.get_env(:elevio, Elevio.API)[:elevio_api_key]
+    Enum.concat(headers,  %{"Authorization": "Bearer #{token}"})
+    |> Enum.concat(%{"x-api-key": key})
+  end
+
+end
